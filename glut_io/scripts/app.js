ce11a1162e94dfb1bc209733753f82a4c2692f36
Qianmeng Chen <qianmengchen@outlook.com>
2017-04-01 08:30:38 -0700

    change workflow

--- glut_io/scripts/app.js
+++ glut_io/scripts/app.js
@@ -1,7 +1,14 @@
 var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+var game;
 
-var game = new Phaser.Game(width, height, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+function login() {
+    gameInit();
+}
+
+function gameInit() {
+    game = new Phaser.Game(width, height, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+}
 
 function preload() {
 }
