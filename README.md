e5da9e436d37f7e7e331c17381735f5c011b81b4
Qianmeng Chen <qianmengchen@outlook.com>
2017-04-01 04:29:53 -0700

    project init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # glut
+### Contributors: Qianmeng Chen, Kevin Qian, Zeyuan Chen, Yizhu Zhang
+This is a project for LA Hacks. Basically it is a typical .io type web game.
---
+++ glut_io/scripts/app.js
@@ -0,0 +1,13 @@
+var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+var game = new Phaser.Game(width, height, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
