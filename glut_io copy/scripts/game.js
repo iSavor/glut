44b196cbe6a647c71991c7ac5156e06e0caa2088
kevinkassimo <kevinkassimo@gmail.com>
2017-04-01 15:34:10 -0700

    Saving... system

---
+++ glut_io copy/scripts/game.js
@@ -0,0 +1,70 @@
+var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+var game;
+var cursors;
+var Game = {};
+
+function login() {
+    gameInit();
+}
+
+function gameInit() {
+    game = new Phaser.Game(width, height, Phaser.AUTO, document.getElementById('game'));
+    game.state.add('Game', Game);
+    game.state.start('Game');
+}
+
+Game.init = function() {
+    game.state.disableVisibilityChange = true;
+    cursors = game.input.keyboard.createCursorKeys();
+}
+
+Game.preload = function() {
+    game.load.image('background', 'assets/background.png');
+    game.load.image('player', 'assets/player.jpg');
+    console.log('preloaded');
+}
+
+Game.create = function() {
+    Game.playerMap = {};
+    var background = game.add.tileSprite(0, 0, 1920, 1920, 'background');
+    game.world.setBounds(0, 0, 1920, 1920);
+    Client.askNewPlayer();
+    console.log('created');
+}
+
+Game.update = function() {
+    Client.slowDown();
+
+    if (cursors.left.isDown) {
+        Client.changeVelo([-20, 0]);
+    } else if (cursors.right.isDown) {
+        Client.changeVelo([20, 0]);
+    } else if (cursors.up.isDown) {
+        Client.changeVelo([0, -20]);
+    } else if (cursors.down.isDown) {
+        Client.changeVelo([0, 20]);
+    }
+
+}
+
+Game.addNewPlayer = function(id, x, y, v) {
+    Game.playerMap[id] = game.add.sprite(x, y, 'player');
+    game.physics.enable(Game.playerMap[id], Phaser.Physics.ARCADE);
+    Game.playerMap[id].body.velocity.x = v[0];
+    Game.playerMap[id].body.velocity.y = v[1];
+};
+
+Game.setCam = function(id){
+    game.camera.follow(Game.playerMap[id]);
+}
+
+Game.removePlayer = function(id) {
+    Game.playerMap[id].destroy();
+    delete Game.playerMap[id];
+};
+
+Game.movePlayer = function(player) {
+    Game.playerMap[player.id].body.velocity.x = player.v[0];
+    Game.playerMap[player.id].body.velocity.y = player.v[1];
+}
---
+++ glut_io copy/server.js
@@ -0,0 +1,82 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+app.use('/styles', express.static(__dirname + '/styles'));
+app.use('/scripts', express.static(__dirname + '/scripts'));
+app.use('/assets', express.static(__dirname + '/assets'));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+server.listen('8080', function() {
+    console.log('Listening on ' + server.address().port);
+});
+
+server.lastPlayderID = 0;
+
+io.on('connection', function(socket) {
+    socket.on('newplayer', function() {
+        socket.player = {
+            id: server.lastPlayderID++,
+            x: randomInt(0, 1920),
+            y: randomInt(0, 1920),
+            v: [0, 0]
+        };
+        socket.emit('allplayers', getAllPlayers());
+        socket.broadcast.emit('newplayer', socket.player);
+        socket.emit('setCam', socket.player.id);
+
+        socket.on('disconnect', function() {
+            io.emit('remove', socket.player.id);
+        });
+
+        socket.on('changeVelo', function(v) {
+            socket.player.v = socket.player.v.map((ele, index) => ele + v[index]);
+            if (socket.player.v[0] > 200) {
+                socket.player.v[0] = 200;
+            } else if (socket.player.v[0] < -200) {
+                socket.player.v[0] = -200;
+            }
+            if (socket.player.v[1] > 200) {
+                socket.player.v[1] = 200;
+            } else if (socket.player.v[1] < -200) {
+                socket.player.v[1] = -200;
+            }
+            io.emit('move', socket.player);
+        });
+
+        socket.on('slowDown', function() {
+            if (socket.player.v[0] != 0) {
+                if (socket.player.v[0] > 0) {
+                    socket.player.v[0] -= 10;
+                } else {
+                    socket.player.v[0] += 10;
+                }
+            }
+            if (socket.player.v[1] != 0) {
+                if (socket.player.v[1] > 0) {
+                    socket.player.v[1] -= 10;
+                } else {
+                    socket.player.v[1] += 10;
+                }
+            }
+            io.emit('move', socket.player);
+        });
+    });
+});
+
+function getAllPlayers() {
+    var players = [];
+    Object.keys(io.sockets.connected).forEach(function(socketID) {
+        var player = io.sockets.connected[socketID].player;
+        if (player) players.push(player);
+    });
+    return players;
+}
+
+function randomInt(low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
