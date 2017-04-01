83a64277aaece6b5c2bbace14c66b940a91207ab
Qianmeng Chen <qianmengchen@outlook.com>
2017-04-01 11:23:31 -0700

    add player

--- glut_io/scripts/game.js
+++ glut_io/scripts/game.js
@@ -8,30 +8,32 @@ function login() {
 }
 
 function gameInit() {
-    game = new Phaser.Game(width, height, Phaser.AUTO, '', { preload: preload, create: create, update: update });
-    //game.state.add('Game', Game);
-    //game.state.start('Game');
+    game = new Phaser.Game(width, height, Phaser.AUTO, document.getElementById('game'));
+    game.state.add('Game', Game);
+    game.state.start('Game');
 }
 
-/*
 Game.init = function(){
     game.state.disableVisibilityChange = true;
 }
 
-Game.create = function(){
-    Game.playerMap = {};
-}
-*/
-
-function preload() {
+Game.preload = function() {
     game.load.image('background', 'assets/background.png');
+    game.load.image('player', 'assets/player.jpg');
     console.log('preloaded');
 }
 
-function create() {
+Game.create = function() {
+    Game.playerMap = {};
     var background = game.add.tileSprite(0, 0, width, height, 'background');
+    Client.askNewPlayer();
     console.log('created');
 }
 
-function update() {
+Game.update = function() {
 }
+
+Game.addNewPlayer = function(id,x,y){
+    console.log(x, y);
+    Game.playerMap[id] = game.add.sprite(x,y,'player');
+};
--- glut_io/server.js
+++ glut_io/server.js
@@ -13,4 +13,32 @@ app.get('/', function(req, res){
 
 server.listen('8080', function(){
     console.log('Listening on ' + server.address().port);
-})
+});
+
+server.lastPlayderID = 0;
+
+io.on('connection',function(socket){
+    socket.on('newplayer',function(){
+        console.log("new player");
+        socket.player = {
+            id: server.lastPlayderID++,
+            x: randomInt(100, 400),
+            y: randomInt(100, 400)
+        };
+        socket.emit('allplayers',getAllPlayers());
+        socket.broadcast.emit('newplayer',socket.player);
+    });
+});
+
+function getAllPlayers(){
+    var players = [];
+    Object.keys(io.sockets.connected).forEach(function(socketID){
+        var player = io.sockets.connected[socketID].player;
+        if(player) players.push(player);
+    });
+    return players;
+}
+
+function randomInt(low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
