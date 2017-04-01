3e77f6ea631fb9898be03da372a8b7317377bb10
Qianmeng Chen <qianmengchen@outlook.com>
2017-04-01 10:45:32 -0700

    background set up

--- glut_io/scripts/game.js
+++ glut_io/scripts/game.js
@@ -9,19 +9,27 @@ function login() {
 
 function gameInit() {
     game = new Phaser.Game(width, height, Phaser.AUTO, '', { preload: preload, create: create, update: update });
-    game.state.add('Game', Game);
-    game.state.start('Game');
+    //game.state.add('Game', Game);
+    //game.state.start('Game');
 }
 
+/*
 Game.init = function(){
     game.state.disableVisibilityChange = true;
 }
 
+Game.create = function(){
+    Game.playerMap = {};
+}
+*/
+
 function preload() {
+    game.load.image('background', 'assets/background.png');
     console.log('preloaded');
 }
 
 function create() {
+    var background = game.add.tileSprite(0, 0, width, height, 'background');
     console.log('created');
 }
 
